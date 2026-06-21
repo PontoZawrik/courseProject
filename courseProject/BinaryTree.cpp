166edca478f70c951c2ac85bb5f5a4e7ea7760a7
PontoZawrik <Psichotroniks@tut.by>
2026-06-22 00:33:26 +0300

    BinaryTree (start/create)

---
+++ courseProject/BinaryTree.cpp
@@ -0,0 +1,72 @@
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree() {}
+BinaryTree::BinaryTree(DataField data) {
+	create_tree(data);
+}
+
+My_TreeNode* BinaryTree::find_el(My_TreeNode* node, size_t id) {
+	if (!node) {
+		return nullptr;
+	}
+	if (node->id == id) {
+		return node;
+	}
+
+	My_TreeNode* left = find_el(node->left, id);
+	if (left) {
+		return left;
+	}
+	return find_el(node->right, id);
+
+}
+
+My_TreeNode* BinaryTree::find_el(size_t id) {
+	if (!root) {
+		return nullptr;
+	}
+	if (root->id == id) {
+		return root;
+	}
+
+	My_TreeNode* left = find_el(root->left, id);
+	if (left) {
+		return left;
+	}
+	return find_el(root->right, id);
+}
+
+bool BinaryTree::create_tree(DataField data) {
+	if (root) {
+		return false;
+	}
+	root = new My_TreeNode;
+	root->data_field = data;
+	root->id = global_id++;
+	root->left = nullptr;
+	root->right = nullptr;
+	return true;
+}
+
+bool BinaryTree::add_node(size_t id, DataField data) {
+	My_TreeNode* parent = find_el(id);
+
+	if (!parent || (parent->left && parent->right)) {
+		return false;
+	}
+
+	My_TreeNode* new_node = new My_TreeNode;
+	new_node->data_field = data;
+	new_node->id = global_id++;
+	new_node->left = nullptr;
+	new_node->right = nullptr;
+
+	if (!parent->left) {
+		parent->left = new_node;
+	}
+	else {
+		parent->right = new_node;
+	}
+
+	return true;
+}
--- courseProject/mainForm.h
+++ courseProject/mainForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "BinaryTree.h"
 
 namespace courseProject {
 
@@ -14,6 +15,8 @@ namespace courseProject {
 	/// </summary>
 	public ref class mainForm : public System::Windows::Forms::Form
 	{
+	private:
+		BinaryTree* tree = new BinaryTree();
 	public:
 		mainForm(void)
 		{
@@ -21,6 +24,14 @@ namespace courseProject {
 			//
 			//TODO: добавьте код конструктора
 			//
+			DataField data;
+			data.bankName = "T-bank";
+			data.cost = 1000000;
+			tree->create_tree(data);
+			tree->add_node(0, data);
+			tree->add_node(0, data);
+			tree->add_node(1, data);
+			RebuildTree(tree);
 		}
 
 	protected:
@@ -34,6 +45,8 @@ namespace courseProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TreeView^ treeView1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +61,57 @@ namespace courseProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"mainForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
+			this->SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this->treeView1->Location = System::Drawing::Point(156, 63);
+			this->treeView1->Name = L"treeView1";
+			this->treeView1->Size = System::Drawing::Size(383, 286);
+			this->treeView1->TabIndex = 0;
+			// 
+			// mainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(963, 511);
+			this->Controls->Add(this->treeView1);
+			this->Name = L"mainForm";
+			this->Text = L"mainForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+		private: bool RebuildTree(BinaryTree* tree) {
+			if (!tree->root) {
+				return false;
+			}
+
+			treeView1->Nodes->Clear();
+
+			TreeNode^ rootNode = gcnew TreeNode(gcnew String(tree->root->data_field.bankName.c_str()));
+			rootNode->Tag = tree->root->id;
+			treeView1->Nodes->Add(rootNode);
+
+			BuildTree(rootNode, tree->root);
+			treeView1->ExpandAll();
+			return true;
+		}
+			
+		void BuildTree(TreeNode^ parentNode, My_TreeNode* node) {
+			if (node == nullptr) {
+				return;
+			}
+
+			TreeNode^ current = gcnew TreeNode(gcnew String(node->data_field.bankName.c_str()));
+			current->Tag = node->id;
+			parentNode->Nodes->Add(current);
+
+			BuildTree(current, node->left);
+			BuildTree(current, node->right);
+		}
+
+
 	};
 }
